ab75c41b68ca009e5a5beaf8e49ee76bcee63468
Hudhayfah Ismail <hoismail2017@gmail.com>
2022-11-06 07:26:29 +0100

    test

--- index.js
+++ index.js
@@ -17,10 +17,11 @@ mongoose.connect(process.env.MONGO_URL, (err, conn) => {
 
 app.use(express.json())
 app.use(cors({
-    origin: "https://getcontries.netlify.app",
+    // origin: "https://getcontries.netlify.app",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true, // allow session cookie from browser to pass through
     optionsSuccessStatus: 200,
+    
 
 }))
 // app.use(cors())
